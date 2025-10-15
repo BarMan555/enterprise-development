1cf6b9d0bd0ae18a5bdeee00b61a54a83e3c2969
boris <boris20033@gmail.com>
2025-10-16 01:40:18 +0300

    Add DTO class for doctor and appointment classes.
    Add DTO class for doctor and appointment classes.

---
+++ Hospital/Hospital.Application/Dto/AppointmentDto.cs
@@ -0,0 +1,34 @@
+using Hospital.Domain.Models;
+
+namespace Hospital.Application.Dto;
+
+/// <summary>
+/// DTO Represents a Appointment for registering a <see cref="Patient"/> with a <see cref="Doctor"/>
+/// </summary>
+public class AppointmentDto
+{
+    /// <summary>
+    /// <see cref="Patient"/> which registering on Appointment
+    /// </summary>
+    public required Patient Patient { get; set; }
+
+    /// <summary>
+    /// <see cref="Doctor"/> for Appointment
+    /// </summary>
+    public required Doctor Doctor { get; set; }
+
+    /// <summary>
+    /// Date of the Appointment
+    /// </summary>
+    public required DateTime AppointmentDateTime { get; set; }
+
+    /// <summary>
+    /// Room of Number where Appointment
+    /// </summary>
+    public required int RoomNumber { get; set; }
+
+    /// <summary>
+    /// Appointment is repeat or not
+    /// </summary>
+    public required bool IsReturnVisit { get; set; }
+}
---
+++ Hospital/Hospital.Application/Dto/DoctorDto.cs
@@ -0,0 +1,29 @@
+using Hospital.Domain.Enums;
+
+namespace Hospital.Application.Dto;
+
+/// <summary>
+/// DTO Represents a Doctor
+/// </summary>
+public class DoctorDto
+{
+    /// <summary>
+    /// Name of the Doctor
+    /// </summary>
+    public required string FullName { get; set; }
+
+    /// <summary>
+    /// Date of Birthday of the Doctor
+    /// </summary>
+    public required DateTime DateOfBirth { get; set; }
+
+    /// <summary>
+    /// Specialization of the Doctor
+    /// </summary>
+    public required Specialization Specialization { get; set; }
+
+    /// <summary>
+    /// Experience of the Doctor
+    /// </summary>
+    public required int ExperienceYears { get; set; }
+}
